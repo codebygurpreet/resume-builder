38fb68ee248823f6aec6ba9e592fc9c10d8aa2a3
codebygurpreet <geekygurpreet4@gmail.com>
2025-12-15 21:34:33 +0530

    feat(ui): implement panels and navigation logic
    - Added state for background and active section tracking
    - Created left-side panels with progress bar
    - Implemented Previous / Next buttons for section navigation

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,17 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
-import { dummyResumeData } from '../assets/assets';
-import { ArrowLeftIcon } from 'lucide-react';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import {
+  ArrowLeftIcon,
+  Briefcase,
+  ChevronLeft,
+  ChevronRight,
+  FileText,
+  FolderIcon,
+  GraduationCap,
+  Sparkles,
+  User,
+} from "lucide-react";
 
 const ResumeBuilder = () => {
-
-  const {resumeId} = useParams();
+  const { resumeId } = useParams();
 
   const [resume, setResumeData] = useState({
-    _id: '',
-    title: '',
+    _id: "",
+    title: "",
     personal_info: {},
-    professional_summary: '',
+    professional_summary: "",
     experience: [],
     education: [],
     projects: [],
@@ -19,34 +28,111 @@ const ResumeBuilder = () => {
     template: "classic",
     accent_color: "#3B82F6",
     public: false,
-  })
+  });
 
   const loadExistingResume = async () => {
-    const resume = dummyResumeData.find(resume => resume._id == resumeId);
-    if(resume){
+    const resume = dummyResumeData.find((resume) => resume._id == resumeId);
+    if (resume) {
       setResumeData(resume);
       document.title = resume.title;
     }
-  }
+  };
 
-  useEffect(()=> {
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [removeBackground, setRemoveBackground] = useState("false");
+
+  const sections = [
+    { id: "personal", name: "personal Info", icon: User },
+    { id: "summary", name: "Summary", icon: FileText },
+    { id: "experince", name: "Experince", icon: Briefcase },
+    { id: "education", name: "Education", icon: GraduationCap },
+    { id: "projects", name: "Projects", icon: FolderIcon },
+    { id: "skills", name: "Skills", icon: Sparkles },
+  ];
+
+  const activeSection = sections[activeSectionIndex];
+
+  useEffect(() => {
     loadExistingResume();
-  },[])
-
+  }, []);
 
   return (
     <div>
       {/* Back to Dashboard */}
-        <div className='max-w-7xl mx-auto px-4 py-6'>
-          <Link to={'/app'} className='inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all'>
-            <ArrowLeftIcon className='size-4' /> Back to Dashboard
-          </Link>
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <Link
+          to={"/app"}
+          className="inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all"
+        >
+          <ArrowLeftIcon className="size-4" /> Back to Dashboard
+        </Link>
+      </div>
+
+      {/* Panels */}
+      <div className="max-w-7xl mx-auto px-4 pb-8">
+        <div className="grid lg:grid-cols-12 gap-8">
+
+          {/* Left Panel - Form */}
+          <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
+            <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
+
+              {/* progress bar using activeSectionIndex */}
+              <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200" />
+              <hr
+                className="absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600 border-none transition-all duration-2000"
+                style={{
+                  width: `${
+                    (activeSectionIndex * 100) / (sections.length - 1)}%` }}
+                    
+              />
+              
+
+              {/* Section Navigation - buttons (template, accent, prev, next)*/}
+              <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
+
+                {/* buttons - template and accent */}
+                <div></div>
+
+
+                {/* buttons - prev and next */}
+                <div className="flex items-center">
+                  { activeSectionIndex !== 0 && (
+                      <button onClick= {()=> setActiveSectionIndex((prevIndex)=>Math.max(prevIndex - 1, 0))}
+                      className="flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all" disabled={activeSectionIndex == 0}>
+                        <ChevronLeft className="size-4"/>
+                        Previous
+                      </button>
+                    )}
+                    <button onClick= {()=> setActiveSectionIndex((prevIndex)=>Math.min(prevIndex + 1, sections.length - 1))}
+                      className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length - 1 && 'opacity-50'}`} disabled={activeSectionIndex == sections.length - 1}>
+                        Next
+                        <ChevronRight className="size-4"/>
+                      </button>
+
+                </div>
+              </div>
+
+              {/* Form Content */}
+              <div className="space-y-6">
+                {activeSection.id == 'personal' && (
+                  <div></div>
+                )}
+              </div>
+
+
+
+            </div>
+          </div>
+
+
+          {/* Right Panel - Preview */}
+          <div></div>
+
 
         </div>
-
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResumeBuilder
+export default ResumeBuilder;
