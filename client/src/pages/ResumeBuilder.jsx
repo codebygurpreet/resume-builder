0fa37a5f908f3ae34957e4119a820eb87b8c2e67
codebygurpreet <geekygurpreet4@gmail.com>
2025-12-17 00:16:40 +0530

    feat(ui): implement resume templates and preview support in resumeBuilder * Added Classic, Minimal, Modern, and MinimalImage templates * Created ResumePreview component for rendering selected templates * Integrated preview functionality into ResumeBuilder

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -13,6 +13,7 @@ import {
   User,
 } from "lucide-react";
 import PersonalInfoForm from "../components/PersonalInfoForm";
+import ResumePreview from "../components/ResumePreview";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -130,7 +131,16 @@ const ResumeBuilder = () => {
 
 
           {/* Right Panel - Preview */}
-          <div></div>
+          <div className="lg:col-span-7 max-lg:mt-6">
+
+            <div>
+              {/* Buttons */}
+            </div>
+
+            {/* resume preview Component */}
+            <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color}/>
+
+          </div>
 
 
         </div>
