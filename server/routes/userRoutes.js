42ae4d8d2da364f7acf345caf298dc70305bd129
codebygurpreet <geekygurpreet4@gmail.com>
2025-12-27 00:37:37 +0530

    feat(server): add getUserResumes controller and route for retrieving user resumes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 // importing required modules
 import express from "express";
-import { registerUser, loginUser, getUserById } from "../controllers/userController.js";
+import { registerUser, loginUser, getUserById, getUserResumes } from "../controllers/userController.js";
 
 // importing authentication middleware
 import protect from "../middlewares/authMiddleware.js";
@@ -12,6 +12,7 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUserById);
+userRouter.get("/resumes", protect, getUserResumes);
 
 // exporting the default user router
 export default userRouter;
