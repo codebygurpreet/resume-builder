475f9c6aa8219bdd39d5f64e3204b00fc7405d88
codebygurpreet <geekygurpreet4@gmail.com>
2025-12-27 16:29:26 +0530

    feat(server): add updateResume controller with ImageKit image upload and bg removal

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,4 +1,6 @@
 import Resume from "../models/Resume.js"
+import imagekit from "../configs/imageKit.js"
+import fs from "fs"
 
 // controller for creating a new resume
 // POST: /api/resumes/create
@@ -89,3 +91,41 @@ export const getPublicResumeById = async (req, res) => {
     }
 }
 
+
+// controller for updateding a resume
+// POST: /api/resumes/update
+export const updateResume = async (req, res) => {
+    try {
+        const userId = req.userId;
+        const { resumeId, resumeData, removeBackground } = req.body;
+        const image = removeBackground.file;
+
+        let resumeDataCopy = JSON.parse(resumeData);
+
+        if (image) {
+
+            const imageBufferData = fs.createReadStream(image.path);
+
+            const response = await imagekit.files.upload({
+                file: imageBufferData,
+                fileName: 'resume.png',
+                folder: 'user-resumes',
+                transformation: {
+                    pre: 'w-300, h-300,fo-face,z-0.75' + (removeBackground ? ',e-bgremove' : '')
+                }
+            });
+            resumeDataCopy.personal_info.image = response.url
+        }
+
+        // find by userId and update resume
+        const resume = await Resume.findByIdAndUpdate(
+            { userId, _id: resumeId }, resumeDataCopy,
+            { new: true });
+
+        // return success message
+        return res.status(200).json({ message: "Saved successfully", resume });
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+}
