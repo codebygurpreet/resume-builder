e6e1cee14c722deb3463d4a1f0ab8abf2e95f189
codebygurpreet <geekygurpreet4@gmail.com>
2025-12-28 19:57:06 +0530

    feat: add Redux for authentication management
    - Installed packages @reduxjs/toolkit, react-redux, axios, and react-pdftotext dependencies.
    - Added authSlice for handling authentication state with login, logout, and loading actions.
    - Configured Redux store to include authReducer.

---
+++ client/src/app/features/authSlice.js
@@ -0,0 +1,29 @@
+// importing required modules
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        token: null,
+        user: null,
+        loading: true
+    },
+    reducers: {
+        login: (state, action)=> {
+            state.token = action.payload.token,
+            state.user = action.payload.user
+        },
+        logout: (state)=> {
+            state.token = "",
+            state.user = null
+        },
+        setLoading: (state, action)=>{
+            state.loading = action.payload
+        }
+    }
+})
+
+export const {login, logout, setLoading} = authSlice;
+
+export default authSlice.reducer;
